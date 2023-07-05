c88b5ede5784bba64858d5f5befde5a6bde76af1
Volodymyr <buvoro@gmail.com>
2023-07-05 18:42:39 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+const toggleBtn = ((...btns) => btns.forEach(i => i.toggleAttribute('disabled')));
+
+const bodyEl = document.querySelector('body')
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+let id;
+
+toggleBtn(btnStopEl);
+
+bodyEl.addEventListener('click', onClick);
+
+function onClick(evt) {
+    if (evt.target === btnStartEl) {
+        toggleBtn(btnStartEl, btnStopEl);
+        id = setInterval(() => bodyEl.style.backgroundColor = getRandomHexColor(), 1000);
+    };
+    if (evt.target === btnStopEl) {
+        toggleBtn(btnStartEl, btnStopEl);
+        clearInterval(id);
+    };
+}
+
+
 
